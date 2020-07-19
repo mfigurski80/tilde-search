944f0a691eb7c2d4bef25d365434cff00472c2ed
Mikolaj Figurski <meek.f80@gmail.com>
2020-07-19 14:20:56 -0400

    Pull out common data interface functions

--- crawler/data.py
+++ crawler/data.py
@@ -1,3 +1,5 @@
+import json
+
 
 def read_sites():
     with open('../discovery/sites.txt') as f:
@@ -10,3 +12,16 @@ def write_sites(data):
     with open('../discovery/sites.txt', 'w') as f:
         f.write(data)
         f.close()
+
+
+def read_json(file, default=None):
+    try:
+        with open(file) as infile:
+            return json.load(infile)
+    except FileNotFoundError as e:
+        return default
+
+
+def write_json(file, obj):
+    with open(file, 'w') as outfile:
+        json.dump(obj, outfile)
--- crawler/init_freq_dict.py
+++ crawler/init_freq_dict.py
@@ -1,6 +1,5 @@
 import urllib.request
 import re
-import json
 from bs4 import BeautifulSoup as bs4
 from pdb import set_trace as tr
 
@@ -21,49 +20,36 @@ def parse_text(html):
     return text
 
 
-def read_json():
-    try:
-        with open('./document_frequency.json') as infile:
-            return json.load(infile)
-    except FileNotFoundError as e:
-        return {
-            'count': 0,
-            'last': None,
-            'data': {}
-        }
-
-
-def write_json(obj):
-    assert set(obj.keys()) == {'count', 'last', 'data'}
-    with open('./document_frequency.json', 'w') as outfile:
-        json.dump(obj, outfile)
-
-
-def make_document_frequency_dict(batches=100):
-    n, last, frequency = read_json().values()
+def make_document_frequency_dict(batches=20):
+    n, last, frequency = read_json(
+        './document_frequency.json',
+        {'count': 0, 'last': None, 'data': {}}
+    ).values()
 
     sites = read_sites()
     if last is not None and n > 0:
         if sites[n-1] == last:
             sites = sites[n:]
+        else:
+            sites = sites[sites.index(last):]
 
     for i, s in enumerate(sites):
         if len(s) <= 0:
             continue
-        print(f'{i}+{n}: s')
+        print(f'{i}+{n}: {s}')
         html = get_site(s)
         text = parse_text(html)
-        tokens = tokenize(text)
+        tokens = list(set(tokenize(text)))
         for word in tokens:
             if word not in frequency:
                 frequency[word] = 0
             frequency[word] += 1
         n += 1
         last = s
-        if i + 1 % batches == 0:  # save in batches
-            write_json({'count': n, 'last': last, 'data': frequency})
+        if (i + 1) % batches == 0:  # save in batches
+            write_json('./document_frequency.json', {'count': n, 'last': last, 'data': frequency})
 
-    write_json({'count': n, 'last': last, 'data': frequency})
+    write_json('./document_frequency.json', {'count': n, 'last': last, 'data': frequency})
 
 
 if __name__ == '__main__':
